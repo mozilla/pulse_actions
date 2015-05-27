d2c8a7111b118250b7e33a43ea978f7f3133d989
Alice <alicescarpa@gmail.com>
2015-05-27 17:24:47 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+deps = [
+    'mozillapulse',
+    'mozci>=0.6.1',
+    'requests',
+]
+
+setup(name='pulse-actions',
+      version='0.1',
+      description='A pulse listener that acts upon messages with mozci.',
+      classifiers=['Intended Audience :: Developers',
+                   'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
+      author='Alice Scarpa',
+      author_email='alicescarpa@gmail.com',
+      license='MPL 2.0',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=deps,
+      url='https://github.com/adusca/pulse_actions'
+      entry_points={
+          'console_scripts': [
+              'run-pulse-actions = pulse-actions.worker:main'
+              ],
+          })
