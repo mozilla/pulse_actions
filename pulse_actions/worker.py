64c33e4a866e44109b80c04091aacaafee930f8f
Alice Scarpa <alicescarpa@gmail.com>
2015-06-17 10:36:14 -0300

    Starting up

--- pulse_actions/worker.py
+++ pulse_actions/worker.py
@@ -82,7 +82,7 @@ def main():
         exchange=options['exchange'],
         topic=options['topic'],
         event_handler=handler_function,
-        dry_run=True)
+        dry_run=False)
 
 if __name__ == '__main__':
     main()
