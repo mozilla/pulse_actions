e5262b38e8c9caa8523055db76075906a17b0ad2
Vaibhav Agrawal <vaibhavmagarwal@gmail.com>
2015-07-27 22:22:33 -0700

    Adding trigger_all_talos_jobs ability in pulse_actions.

--- pulse_actions/handlers/treeherder_resultset.py
+++ pulse_actions/handlers/treeherder_resultset.py
@@ -1,4 +1,4 @@
-from mozci.mozci import trigger_missing_jobs_for_revision
+from mozci.mozci import trigger_missing_jobs_for_revision, trigger_all_talos_jobs
 from thclient import TreeherderClient
 from pulse_actions.publisher import MessageHandler
 MEMORY_SAVING_MODE = True
@@ -6,6 +6,7 @@ MEMORY_SAVING_MODE = True
 def on_resultset_action_event(data, message, dry_run):
     repo_name = data["project"]
     action = data["action"]
+    times = data["times"]
     # Pulse gives us resultset_id, we need to get revision from it.
     resultset_id = data["resultset_id"]
     treeherder_client = TreeherderClient()
@@ -18,7 +19,12 @@ def on_resultset_action_event(data, message, dry_run):
             status = 'trigger_missing_jobs request sent'
         else:
             status = 'Dry-mode, no request sent'
-
+    elif action == "trigger_all_talos_jobs":
+        trigger_all_talos_jobs(repo_name, revision, times, dry_run=dry_run)
+        if not dry_run:
+            status = 'trigger_all_talos_jobs %s times request sent' % times
+        else:
+            status = 'Dry-mode, no request sent'
     # Send a pulse message showing what we did
     message_sender = MessageHandler()
     pulse_message = {
