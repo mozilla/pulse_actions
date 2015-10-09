d2e875d3e706ae7980a28425a0f93a0d62286800
Armen Zambrano G <armenzg@mozilla.com>
2015-10-09 11:32:11 -0400

    Revert "Add debugging information"
    This reverts commit f42bc6ff359021827ccecd46536bdcacb28cc0e2.

--- pulse_actions/handlers/backfilling.py
+++ pulse_actions/handlers/backfilling.py
@@ -36,11 +36,8 @@ def on_event(data, message, dry_run):
     buildjson.BUILDS_CACHE = {}
     query_jobs.JOBS_CACHE = {}
     payload = data["payload"]
-    status = payload.get("status")
-    if not status:
-        LOG.warning("DEBUG_THIS")
-        LOG.info(payload)
-    buildername = payload.get("buildername")
+    status = payload["status"]
+    buildername = payload["buildername"]
 
     # Backfill a failed job
     if status in [FAILURE, WARNING]:
@@ -65,5 +62,6 @@ def on_event(data, message, dry_run):
             trigger_build_if_missing=False
         )
     else:
-        LOG.debug("'%s' with status %i. Nothing to be done.",
+        # TODO: change this to debug after a testing period
+        LOG.info("'%s' with status %i. Nothing to be done.",
                  buildername, status)
