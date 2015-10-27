2fba2feccde0b556bf20d9d1c96455bfbf232ad2
Armen Zambrano G <armenzg@mozilla.com>
2015-10-27 12:48:00 -0400

    Share formatter

--- pulse_actions/worker.py
+++ pulse_actions/worker.py
@@ -87,32 +87,25 @@ def setup_logging(logging_level):
     LOG = logging.getLogger()
     LOG.setLevel(logging.DEBUG)
 
-    if logging_level == logging.DEBUG:
-        format = '%(asctime)s %(name)s\t %(levelname)s:\t %(message)s'
-    else:
-        format = '%(levelname)s:\t %(message)s'
-
+    format = '%(asctime)s %(name)s\t %(levelname)s:\t %(message)s'
+    formatter = logging.Formatter(format, datefmt='%H:%M:%S')
     # Handler 1 - Store all INFO messages in a specific file
     # This logging.txt file will *only* show messages for that same day as Heroku
     # dynos restart every day - Use papertrail for more details.
     fh = logging.FileHandler('logging.txt')
     fh.setLevel(logging.INFO)
-    formatter = logging.Formatter(format, datefmt='%H:%M:%S')
     fh.setFormatter(formatter)
     LOG.addHandler(fh)
 
     # Handler 2 - Store all DEBUG messages in a specific file
     fh = logging.FileHandler('logging_debug.txt')
     fh.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(format, datefmt='%H:%M:%S')
     fh.setFormatter(formatter)
     LOG.addHandler(fh)
 
     # Handler 3 - Output to console (this is the output for Papertrail)
     console = logging.StreamHandler()
     console.setLevel(logging_level)
-    # console does not use the same formatter specified in basicConfig we set it again
-    formatter = logging.Formatter('%(name)s\t %(levelname)s:\t %(message)s')
     console.setFormatter(formatter)
     LOG.addHandler(console)
 
