302c0024265bc16f66c6693dabd764a936612351
Armen Zambrano G <armenzg@mozilla.com>
2016-01-19 17:28:02 -0500

    Fix many issues
    * We now discard invalid builders since I belive it caused these messages [1]
    * Fixed determining if users could schedule jobs or not
    ** This prevented many developers from using the tool at all
    * Lots of other improvements to help debug issues in the future
    
    [1]
    ```
    Jan 14 04:41:21 pulse-actions app/worker2.1:   File "/app/pulse_actions/handlers/treeherder_runnable.py", line 70, in on_runnable_job_event
    Jan 14 04:41:21 pulse-actions app/worker2.1:     complete=False)
    Jan 14 04:41:21 pulse-actions app/worker2.1:   File "/app/.heroku/python/lib/python2.7/site-packages/mozci/sources/buildbot_bridge.py", line 159, in buildbot_graph_builder
    Jan 14 04:41:21 pulse-actions app/worker2.1:     if is_downstream(b):
    Jan 14 04:41:21 pulse-actions app/worker2.1:   File "/app/.heroku/python/lib/python2.7/site-packages/mozci/platforms.py", line 54, in is_downstream
    Jan 14 04:41:21 pulse-actions app/worker2.1:     return get_buildername_metadata(buildername)['downstream']
    Jan 14 04:41:21 pulse-actions app/worker2.1: TypeError: 'NoneType' object has no attribute '__getitem__'
    ```

--- pulse_actions/handlers/treeherder_runnable.py
+++ pulse_actions/handlers/treeherder_runnable.py
@@ -1,15 +1,16 @@
+import copy
 import logging
 
-from mozci.ci_manager import TaskClusterBuildbotManager
-from mozci.sources import buildjson, buildbot_bridge
-from mozci.mozci import trigger_job
 from mozci import query_jobs
-from thclient import TreeherderClient
+from mozci.ci_manager import TaskClusterBuildbotManager
+from mozci.mozci import trigger_job, valid_builder
+from mozci.sources import buildjson, buildbot_bridge
 from pulse_actions.publisher import MessageHandler
+from thclient import TreeherderClient
 
-logging.basicConfig(format='%(levelname)s:\t %(message)s')
-LOG = logging.getLogger()
+LOG = logging.getLogger('extend')
 MEMORY_SAVING_MODE = True
+TREEHERDER = 'https://treeherder.mozilla.org/#/jobs?repo=%(repo)s&revision=%(revision)s'
 
 
 def on_runnable_job_stage_event(data, message, dry_run):
@@ -28,29 +29,38 @@ def on_runnable_job_event(data, message, dry_run, stage):
     # Cleaning mozci caches
     buildjson.BUILDS_CACHE = {}
     query_jobs.JOBS_CACHE = {}
-    repo_name = data["project"]
-    requester = data["requester"]
-    resultset_id = data["resultset_id"]
-    buildernames = data["buildernames"]
 
     if stage:
         treeherder_client = TreeherderClient(host='treeherder.allizom.org')
     else:
         treeherder_client = TreeherderClient()
 
-    mgr = TaskClusterBuildbotManager()
+    # Grabbing data received over pulse
+    repo_name = data["project"]
+    requester = data["requester"]
+    resultset_id = data["resultset_id"]
+    buildernames = data["buildernames"]
 
-    LOG.info("New jobs requested by %s on repo_name %s with resultset_id: %s" %
-             (data["requester"], data["project"], data["resultset_id"]))
+
     resultset = treeherder_client.get_resultsets(repo_name, id=resultset_id)[0]
     revision = resultset["revision"]
     author = resultset["author"]
     status = None
 
+    treeherder_link = TREEHERDER % {'repo': repo_name, 'revision': resultset['revision']}
+
+    LOG.info("New jobs requested by %s for %s" % (requester, treeherder_link))
+    LOG.debug('List of builders: %s' % str(buildernames))
+
     message_sender = MessageHandler()
     # Everyone can press the button, but only authorized users can trigger jobs
     # TODO: remove this when proper LDAP identication is set up on TH
-    if author != requester or not _whitelisted_users(requester):
+    if not (
+        requester.endswith('@mozilla.com') or \
+        author == requester or \
+        _whitelisted_users(requester)):
+
+        # Remove message from pulse queue
         message.ack()
 
         # We publish a message saying we will not trigger the job
@@ -59,30 +69,59 @@ def on_runnable_job_event(data, message, dry_run, stage):
             'requester': requester,
             'status': "Could not determine if the user is authorized, nothing was triggered."}
         routing_key = '{}.{}'.format(repo_name, 'runnable')
-        message_sender.publish_message(pulse_message, routing_key)
+        try:
+            message_sender.publish_message(pulse_message, routing_key)
+        except:
+            LOG.error("Failed to publish message over pulse stream.")
 
-        raise Exception("Requester %s is not allowed to trigger jobs." %
-                        requester)
+        LOG.error("Requester %s is not allowed to trigger jobs." % requester)
+        return # Raising an exception adds too much noise
 
-    builders_graph, ready = buildbot_bridge.buildbot_graph_builder(buildernames,
-                                                                   revision,
-                                                                   complete=False)
+    # Discard invalid builders
+    # Until https://github.com/mozilla/mozilla_ci_tools/issues/423 is fixed
+    invalid_builders = []
+    for b in buildernames:
+        if not valid_builder(b):
+            invalid_builders.append(b)
+            buildernames.remove(b)
 
-    mgr.schedule_graph(
-        repo_name=repo_name,
+    if invalid_builders:
+        LOG.info('Invalid builders: %s' % str(invalid_builders))
+
+    builders_graph, other_builders_to_schedule = buildbot_bridge.buildbot_graph_builder(
+        builders=buildernames,
         revision=revision,
-        metadata={
-            'name': 'pulse_actions_graph',
-            'description':
-                'Adding new jobs to push via pulse_actions/treeherder for %s.' % requester,
-            'owner': requester,
-            'source': revision,
-        },
-        builders_graph=builders_graph,
-        dry_run=dry_run)
+        complete=False # XXX: This can be removed when BBB is in use
+    )
 
-    for buildername in ready:
-        trigger_job(revision, buildername)
+    if builders_graph != {}:
+        mgr = TaskClusterBuildbotManager()
+        mgr.schedule_graph(
+            repo_name=repo_name,
+            revision=revision,
+            metadata={
+                'name': 'pulse_actions_graph',
+                'description':
+                    'Adding new jobs to push via pulse_actions/treeherder for %s.' % requester,
+                'owner': requester,
+                'source': revision,
+            },
+            builders_graph=builders_graph,
+            dry_run=dry_run)
+    else:
+        LOG.info("We don't have anything to schedule through TaskCluster")
+
+    if other_builders_to_schedule:
+        # XXX: We should be able to replace this once all Buildbot jobs run through BBB
+        # XXX: There might be a work around with https://github.com/mozilla/mozilla_ci_tools/issues/424
+        LOG.info("We're going to schedule these builders via Buildapi: %s" %
+                 str(other_builders_to_schedule))
+        # This is used for test jobs which need an existing Buildbot job to be scheduled
+        for buildername in other_builders_to_schedule:
+            trigger_job(revision, buildername)
+    else:
+        LOG.info("We don't have anything to schedule through Buildapi")
+
 
     # Send a pulse message showing what we did
     message_sender = MessageHandler()
@@ -92,7 +131,10 @@ def on_runnable_job_event(data, message, dry_run, stage):
         'requester': requester,
         'status': status}
     routing_key = '{}.{}'.format(repo_name, 'runnable')
-    message_sender.publish_message(pulse_message, routing_key)
+    try:
+        message_sender.publish_message(pulse_message, routing_key)
+    except:
+        LOG.error("Failed to publish message over pulse stream.")
 
     # We need to ack the message to remove it from our queue
     message.ack()
