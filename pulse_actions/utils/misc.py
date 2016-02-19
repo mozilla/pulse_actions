d5323e8737a1be512b5a98161a2b505920922a57
Armen Zambrano G <armenzg@mozilla.com>
2016-02-19 14:54:53 -0500

    Bug 1249636 - Whitelist Iggi

--- pulse_actions/utils/misc.py
+++ pulse_actions/utils/misc.py
@@ -17,6 +17,7 @@ def whitelisted_users(requester):
     return requester in (
         'aleth@instantbird.org',
         'archaeopteryx@coole-files.de',
+        'mozilla@digitalimagecorp.de',
         'nigelbabu@gmail.com',
         'philringnalda@gmail.com',
     )
